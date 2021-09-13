2c11c7bcc174113a35bfcd8a7dbe6bac3fe697d1
dianeyasminec <dianeyasminec60@gmail.com>
2021-09-13 17:32:49 -0400

    commit tree 3981adfe16698a4c5c6fe6a403cac2302a55a2a3

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, string){
+      return drivers.filter(name=> {
+         return name.toLowerCase()===string.toLowerCase()
+         
+     })
+    
+}
+function fuzzyMatch(drivers, string){
+    return drivers.filter(name => {
+        return name.toLowerCase().indexOf(string.toLowerCase())===0
+    })
+}
+
+function matchName(drivers,string){
+ return drivers.filter(driver => {
+     return driver.name.toLowerCase()===string.toLowerCase()
+ })
+}
